97af0fefde6f814522ad5fd4022583d6c80b26f1
Jackson Ward <jackson.ward21@gmail.com>
2018-10-09 14:55:36 -0500

    Added support for on-campus eatery menus and cleaned up code.

--- index.js
+++ index.js
@@ -7,22 +7,66 @@ var app = express();
 var Nightmare = require('nightmare');
 
 const botId = "3b893bec87836682b56e1358bc";
-const htmlLoc = (__dirname + '/saveFile.html');
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+//This section below runs at startup to fetch static menus of on-campus eateries
+var locs = ['Au Bon Pain', 'Where the Wild Greens AR',
+'Sushi with Gusto', 'Melt Lab', 'Pei Wei', 'Chick-Fil-A', 'Einstein Brother\'s Bagels',
+'Peabody Perks', 'Quiznos', 'Rocket Taco', 'Starbucks', 'True Burger'];
+
+var localMenus = {};
+
+function initLocalMenus(loc) {
+  var loc = loc;
+  localMenus[loc.toLowerCase()] = {};
+  var nightmare = Nightmare({
+  });
+  nightmare
+  .goto('https://www.dineoncampus.com/razorbacks/todays-menu')
+  .wait('.form-control')
+  .click('.form-control')
+  .type('.form-control', `${loc}\u000d`)
+  .wait('.category-name')
+  .wait('.nav-tabs')
+  .wait(1000)
+  .evaluate(function(localMenus, loc) {
+    var categories = document.querySelectorAll('.category-name');
+    categories.forEach(function(category) {
+      var cat = category.textContent.slice(7, (category.textContent.length - 5));
+      localMenus[loc.toLowerCase()][cat.toLowerCase()] = [];
+      var table = category.nextElementSibling.children[0].children[1];
+      var items = table.children.length;
+      for (i = 0; i < items; i++) {
+        menuText = table.children[i].children[0].children[0].children[0].textContent;
+        if (!(localMenus[loc.toLowerCase()][cat.toLowerCase()].includes(menuText))) {
+          localMenus[loc.toLowerCase()][cat.toLowerCase()].push(menuText.slice(9, (menuText.length-7)));
+        }
+      }
+    })
+    return localMenus;
+  }, localMenus, loc)
+  .end((result) => {
+    //Add individual eatery menu to localMenus object
+    localMenus = Object.assign(localMenus, result);
+    console.log("got another menu");
+  })
+  .catch(error => {
+    console.error('Search failed:', error)
+  })
+}
+
+locs.forEach(function(el) {
+  initLocalMenus(el);
+})
+
 function getMenu(location, mealTime) {
   var nightmare = Nightmare({
-    // openDevTools: {
-    //   mode: 'detach'
-    // },
-    show: true
   });
   var loc;
   var menu;
-  console.log(mealTime);
   switch (location) {
     case "fulbright":
     loc = "Fulbright Dining Hall";
@@ -73,7 +117,6 @@ function getMenu(location, mealTime) {
   .wait(1000)
   .evaluate((mealTime) => {
     var tabs = [];
-    console.log(mealTime)
     for (i = 0; i < document.querySelector('.nav-tabs').children.length; i++) {
       tabs.push(document.querySelector('.nav-tabs').children[i]);
     }
@@ -86,9 +129,6 @@ function getMenu(location, mealTime) {
   .wait(3000)
   .evaluate(function(menu) {
     var categories = document.querySelectorAll('.category-name');
-    var found1 = false;
-    var found2 = false;
-    var found3 = false;
     var menuOptions = [];
     menu.forEach(function(menuItem) {
       categories.forEach(function(category) {
@@ -104,22 +144,9 @@ function getMenu(location, mealTime) {
         }
       })
     })
-    // categories.forEach(function(category) {
-    //   menu.forEach(function(menuItem) {
-    //     if (category.textContent.includes(menuItem)) {
-    //       var table = category.nextElementSibling.children[0].children[1];
-    //       var items = table.children.length;
-    //       for (i = 0; i < items; i++) {
-    //         menuOptions.push(table.children[i].children[0].children[0].children[0].textContent)
-    //       }
-    //     }
-    //   })
-    // })
     return menuOptions;
   }, menu)
   .end((menuOptions) => {
-    console.log(menu);
-    console.log(menuOptions);
     if (menuOptions == null) {
       request.post(
         "https://api.groupme.com/v3/bots/post",
@@ -153,7 +180,7 @@ app.post('/', function(req, res) {
   if (!(sender == "hungryBot")) {
     if (message.startsWith(".")) {
       var loc, time;
-      if (message.startsWith(".fulbright")) {
+      if (message.startsWith(".fulbright") || message.startsWith(".fullbright")) {
         loc = 'fulbright';
         if (message.includes('breakfast')) {
           time = 'breakfast';
@@ -176,6 +203,7 @@ app.post('/', function(req, res) {
         if (message.includes('dindin')) {
           time = 'dindin';
         }
+        getMenu(loc, time);
       }
     if (message.startsWith(".brough")) {
       loc = 'brough';
@@ -200,8 +228,29 @@ app.post('/', function(req, res) {
       if (message.includes('dindin')) {
         time = 'dindin';
       }
-    }
       getMenu(loc, time);
+    } else {
+      locs.forEach(function(el) {
+        if (message.startsWith('.' + el.toLowerCase())) {
+          var foundKey = false;
+          for (var key in localMenus[el.toLowerCase()]) {
+            if (message.includes(key.toLowerCase())) {
+              foundKey = true;
+              request.post(
+                "https://api.groupme.com/v3/bots/post",
+                { json: { "bot_id": botId, "text": `${capitalizeFirstLetter(el)} has the following ${key}: ${localMenus[el.toLowerCase()][key].join(', ')}.`}}
+              );
+            }
+          }
+          if (foundKey == false) {
+            request.post(
+              "https://api.groupme.com/v3/bots/post",
+              { json: { "bot_id": botId, "text": `Please add one of the following menu categories to your search: ${Object.getOwnPropertyNames(localMenus[el.toLowerCase()]).join(', ')}`}}
+            );
+          }
+        }
+      })
+    }
     }
   }
 })
